865adc10f6ac1e7b0cf9f36560ac8d7e64987eaf
Landon Clark <landon.d.clark@gmail.com>
2021-06-04 09:48:48 -0600

    Update Struct.cs

--- Esapi/Struct.cs
+++ Esapi/Struct.cs
@@ -1,8 +1,9 @@
-﻿namespace AutoCrop
+namespace AutoRing_SIB
 {
     public class Struct
     {
         public string StructureId { get; set; }
         public double StructureVolume { get; set; }
+        public bool CanModify { get; set; }
     }
 }
