7bdf85b4213aa7b9991ec7902d0404d2c4e83f90
Landon Clark <landon.d.clark@gmail.com>
2021-06-04 09:47:38 -0600

    Update IEsapiService.cs

--- Esapi/IEsapiService.cs
+++ Esapi/IEsapiService.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
-using VMS.TPS.Common.Model.API;
-using VMS.TPS.Common.Model.Types;
 
-namespace AutoCrop
+namespace AutoRing_SIB
 {
     public interface IEsapiService
     {
         Task<StructSet[]> GetStructureSetsAsync();
         Task<Struct[]> GetStructureIdsAsync(string structureSet);
+        Task<string> GetEditableRingNameAsync(string structureSetId, string ringId);
         Task AddRingAsync(string structureSetId, string ptvId, string ringId, double innerMargin, double outerMargin);
         Task CleanUpRingsAsync(string structureSetId, string ptvHighId, string ptvMidId, string ptvLowId, string ptv4Id, string ringHighId, string ringMidId, string ringLowId, string ring4Id);
     }
