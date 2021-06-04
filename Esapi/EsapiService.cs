890aa800b98cb6feaffc4c76b04f65971fcf9d66
Landon Clark <landon.d.clark@gmail.com>
2021-06-04 08:57:06 -0600

    Update EsapiService.cs

--- Esapi/EsapiService.cs
+++ Esapi/EsapiService.cs
@@ -1,11 +1,10 @@
-﻿using System.Linq;
+using System.Linq;
 using System.Threading.Tasks;
 using EsapiEssentials.Plugin;
 using VMS.TPS.Common.Model.API;
 using System;
-using VMS.TPS.Common.Model.Types;
 
-namespace AutoCrop
+namespace AutoRing_SIB
 {
     public class EsapiService : EsapiServiceBase<PluginScriptContext>, IEsapiService
     {
@@ -25,7 +24,8 @@ namespace AutoCrop
                     CreationDate = x.HistoryDateTime,
                     ImageId = x.Image.Id,
                     StructureSetId = x.Id,
-                    StructureSetIdWithCreationDate = x.Id + " - " + x.HistoryDateTime.ToString()
+                    StructureSetIdWithCreationDate = x.Id + " - " + x.HistoryDateTime.ToString(),
+                    CanModify = Helpers.CheckStructureSet(context.Patient, x)
                 })
                 .ToArray();
             });
@@ -38,10 +38,63 @@ namespace AutoCrop
                 .Structures.Where(x => x.Id.ToUpper().Contains("PTV") == true).Select(x => new Struct
                 {
                     StructureId = x.Id,
-                    StructureVolume = x.Volume
+                    StructureVolume = x.Volume,
+                    //CanModify = Helpers.CheckStructure(x)
                 })
                 .ToArray();
             });
+
+        public Task<string> GetEditableRingNameAsync(string structureSetId, string ringId) =>
+            RunAsync(context => GetEditableRingName(context.Patient, structureSetId, ringId));
+
+        public string GetEditableRingName(Patient patient, string structureSetId, string ringId)
+        {
+            StructureSet structureSet = patient.StructureSets.FirstOrDefault(x => x.Id == structureSetId);
+            bool ringInStructureSet = false;
+            bool possibleInStructureSet = false;
+            bool canEditRing = true;
+            string possibleStructureId = String.Empty;
+            foreach (var structure in structureSet.Structures)
+            {
+                if (structure.Id == ringId)
+                {
+                    ringInStructureSet = true;
+                    if (Helpers.CheckStructure(structure) == true) //ring is present but editable
+                        return structure.Id;
+                    else
+                        canEditRing = false;
+                }           
+            }
+            if (ringInStructureSet == true && canEditRing == false) //ring is present but not editable
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    foreach (var structure in structureSet.Structures)
+                    {
+                        if (structure.Id == ringId + i.ToString())  //possible already present
+                        {
+                            possibleInStructureSet = true;
+                            if (Helpers.CheckStructure(structure) == true) //possible is editable
+                                return structure.Id;
+                            else
+                            {
+                                possibleInStructureSet = false;
+                                possibleStructureId = ringId + (i + 1).ToString();
+                                break;
+                            }
+
+                        }
+                        else
+                            possibleStructureId = ringId + i.ToString();
+                    }
+                    if (possibleInStructureSet == false) //possible not in structure and can be added
+                        return possibleStructureId;                     
+                }
+            }
+            if (ringInStructureSet == false) //ring not in structure set and can be added
+                return ringId;
+            throw new Exception("Too many uneditable rings in structure set.");
+        }
 
         public Task AddRingAsync(string structureSetId, string ptvId, string ringId, double innerMargin, double outerMargin) =>
             RunAsync(context => AddRing(context.Patient, structureSetId, ptvId, ringId, innerMargin, outerMargin));
